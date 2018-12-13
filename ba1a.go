cb2286de7cfaabf779d41db8501b8de3e533d476
C Reid <charlesreid1@Gmail.com>
2018-12-13 15:08:48 -0800

    restructure ba1a.go problem - include BA1A() and BA1ADescription()

--- ba1a.go
+++ ba1a.go
@@ -2,16 +2,39 @@ package main
 
 import "fmt"
 
-// Rosalind: Problem BA1A
-//
-// To run:
-//
-// $ go run ba1a.go
+// Rosalind: Problem BA1A: Most Frequent k-mers
 
-func pattern_count(input string, pattern string) int {
+// Describe the problem
+func BA1ADescription() {
+    description := []string{
+        "-----------------------------------------",
+        "Rosalind: Problem BA1A:",
+        "Most Frequest k-mers",
+        "",
+        "Given an input string and a length k,",
+        "report the k-mer or k-mers that occur",
+        "most frequently.",
+        "",
+        "URL: http://rosalind.info/problems/ba1a/",
+        "",
+    }
+    for _, line := range description {
+        fmt.Println(line)
+    }
+}
+
+// Count occurrences of a substring pattern 
+// in a string input
+func PatternCount(input string, pattern string) int {
 
     // Number of substring overlaps
     var overlap = len(input) - len(pattern) + 1
+
+    // If overlap < 1, we are looking 
+    // for a pattern longer than our input
+    if overlap<1 {
+        return 0
+    }
 
     // Count of occurrences
     count:=0
@@ -29,10 +52,10 @@ func pattern_count(input string, pattern string) int {
     return count
 }
 
-func main() {
-    // Call the pattern_count function
-    fmt.Println("Number of occurrences of GCG in GCGCG:")
-    res := pattern_count("GCGCG","GCG")
-    fmt.Println(res)
+// Describe the problem, and call the function
+func BA1A() {
+    BA1ADescription()
+    res := PatternCount("GCGCG","GCG")
+    fmt.Println("PatternCount(GCGCG,GCG) yields:",res)
 }
 
