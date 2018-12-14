92cd45b7abc729fc4ef5dcf783b7f9141e9e6146
C Reid <charlesreid1@Gmail.com>
2018-12-14 01:40:08 -0800

    add code to BA1A test to load test case from file

--- ba1a_test.go
+++ ba1a_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
     "fmt"
+    "strings"
+    "strconv"
+    "io/ioutil"
     "testing"
 )
 
@@ -55,3 +58,50 @@ func TestPatternCounts(t *testing.T) {
         }
     }
 }
+
+
+// Load a PatternCount test (input and output)
+// from a file. Run the test with the input
+// and verify the output matches the output
+// contained in the file.
+func TestPatternCountFile(t *testing.T) {
+    input, pattern, output := GetPatternCountFileContents()
+    int_output, _ := strconv.Atoi(output)
+    result := PatternCount(input, pattern)
+    if result != int_output {
+        err := fmt.Sprintf("Error testing PatternCount using test case from file: results do not match:\rcomputed result = %d\nexpected output = %d",result,int_output)
+        t.Error(err)
+    }
+}
+
+
+// Load the contents of the PatternCount file
+// and return the inputs and outputs for that
+// pattern count test.
+func GetPatternCountFileContents() (string,string,string) {
+    // Read the contents of the input file
+    // into a single string
+    dat, err := ioutil.ReadFile("PatternCount.txt")
+    check(err)
+    contents := string(dat)
+
+    // Buncha index algebra
+    ix_input_start  := strings.Index(contents,"Input")
+    ix_input_end    := ix_input_start + len("Input")
+    ix_output_start := strings.Index(contents,"Output")
+    ix_output_end   := ix_output_start + len("Output")
+    ix_file_end     := len(contents)
+
+    input_contents  := strings.Split(contents[ix_input_end:ix_output_start],"\n")
+    input_contents   = input_contents[1:len(input_contents)-1]
+
+    output_contents := strings.Split(contents[ix_output_end:ix_file_end],"\n")
+    output_contents  = output_contents[1:len(output_contents)-1]
+
+    // Two inputs:
+    // input_contents[0:1]
+    // One output:
+    // output_contents[0]
+    return input_contents[0], input_contents[1], output_contents[0]
+}
+
