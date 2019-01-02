ee90235aa930bef785eec32236843a740a606bca
C Reid <charlesreid1@Gmail.com>
2019-01-02 08:43:43 -0800

    add test for genome reconstruction

--- rosalind/rosalind_ba3.go
+++ rosalind/rosalind_ba3.go
@@ -3,6 +3,7 @@ package rosalind
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 ////////////////////////////////
@@ -29,4 +30,61 @@ func KmerComposition(input string, k int) ([]string, error) {
 
 	// Return the string slice
 	return result, nil
+}
+
+////////////////////////////////
+// BA3b
+
+func ReconstructGenomeFromPath(contigs []string) (string, error) {
+
+	pieces := []string{}
+	for i := 0; i < len(contigs)-1; i++ {
+		pattern1 := contigs[i]
+		pattern2 := contigs[i+1]
+
+		// Stride left-hand string and find where
+		// it lines up to right-hand string
+		overlap_index1 := -1
+		overlap_index2 := -1
+		for i := 0; i < len(pattern1); i++ {
+
+			// Left-hand string: backwards-sliding window
+			start1 := i           // sliding
+			end1 := len(pattern1) // fixed
+			slice1 := pattern1[start1:end1]
+
+			// Right-hand string: fixed shrinking window
+			start2 := 0                 // fixed
+			end2 := (end1 - start2) - i // sliding
+			slice2 := pattern2[start2:end2]
+
+			if slice1 == slice2 {
+				// Many Bothans died to discover this algorithm.
+				overlap_index1 = start1
+				overlap_index2 = end2
+				break
+			}
+		}
+
+		if overlap_index1 < 0 {
+			msg := fmt.Sprintf("Error: ReconstructGenomeFromPath(): No overlap detected between %s and %s\n",
+				pattern1, pattern2)
+			return "", errors.New(msg)
+		}
+
+		// Add on the prefix of the left-hand piece - that's the part
+		// that doesn't overlap with the next right-hand piece.
+		pieces = append(pieces, pattern1[:overlap_index1])
+
+		// Once we're on the last pair of pieces,
+		// include the suffix of the left-hand piece -
+		// the part that overlaps with the next right-hand piece -
+		// then add the suffix of the right-hand piece
+		// (the part that doesn't overlap with the previous
+		// left-hand piece).
+		if i == len(contigs)-2 {
+			pieces = append(pieces, pattern1[overlap_index1:]+pattern2[overlap_index2:])
+		}
+	}
+	return strings.Join(pieces, ""), nil
 }
--- rosalind/rosalind_ba3_test.go
+++ rosalind/rosalind_ba3_test.go
@@ -3,6 +3,7 @@ package rosalind
 import (
 	"fmt"
 	"sort"
+	"strings"
 	"testing"
 )
 
@@ -26,6 +27,78 @@ func TestKmerComposition(t *testing.T) {
 	if !EqualStringSlices(results, gold) {
 		msg := fmt.Sprintf("Error testing KmerComposition()\ncomputed = %v\ngold = %v",
 			results, gold)
+		t.Error(msg)
+	}
+}
+
+/// func TestKmerCompositionFile(t *testing.T) {
+///
+/// 	filename := "data/string_composition.txt"
+///
+/// 	// Read the contents of the input file
+/// 	// into a single string
+/// 	lines, err := ReadLines(filename)
+/// 	if err != nil {
+/// 		log.Fatalf("ReadLines: %v", err)
+/// 	}
+///
+/// 	// Input file contents
+/// 	// lines[0]: Input
+/// 	k_str := lines[1]
+/// 	k, _ := strconv.Atoi(k_str)
+///
+/// 	input := lines[2]
+///
+/// 	// lines[3]: Output
+///
+/// 	// Make space for DNA strings
+/// 	gold := make([]string, len(lines)-4)
+/// 	iLstart := 2
+/// 	iLend := len(lines) - 2
+/// 	// Two counters:
+/// 	// one for the line index (iL),
+/// 	// one for the array index (iA).
+/// 	for iA, iL := 0, iLstart; iL < iLend; iA, iL = iA+1, iL+1 {
+/// 		gold[iA] = lines[iL]
+/// 	}
+///
+/// 	results, err := KmerComposition(input, k)
+/// 	if err != nil {
+/// 		t.Error(fmt.Sprintf("Error: %v", err))
+/// 	}
+///
+/// 	// Sort before comparing
+/// 	sort.Strings(gold)
+/// 	sort.Strings(results)
+///
+/// 	if !EqualStringSlices(results, gold) {
+/// 		for i, j := 0, 0; i < len(results); i, j = i+1, j+1 {
+/// 			if results[i] != gold[j] {
+/// 				fmt.Printf("rslt[i] = rslt[%d] = %s\ngold[j] = gold[%d] = %s\n\n",
+/// 					i, results[i], j, gold[j])
+/// 			}
+/// 		}
+/// 		msg := fmt.Sprintf("Error testing KmerComposition() from file %s:\ncomputed = %d\ngold = %d",
+/// 			filename,
+/// 			len(results), len(gold))
+/// 		t.Error(msg)
+/// 	}
+/// }
+
+/////////////////////////////////
+// BA3b Test
+
+func TestReconstructGenomeFromPath(t *testing.T) {
+	contigs := []string{"ACCGA", "CCGAA", "CGAAG", "GAAGC", "AAGCT"}
+	gold := "ACCGAAGCT"
+
+	results, err := ReconstructGenomeFromPath(contigs)
+	if err != nil {
+		t.Error(err)
+	}
+	if results != gold {
+		msg := fmt.Sprintf("Error testing ReconstructGenomeFromPath():\ninputs = %s\ncomputed = %s\ngold     = %s",
+			strings.Join(contigs, " "), results, gold)
 		t.Error(msg)
 	}
 }
