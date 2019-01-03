d10d4e247bc63405c7748ad5befe04dc7ec242e9
C Reid <charlesreid1@Gmail.com>
2019-01-03 11:46:30 -0800

    update and fix string composition test

--- rosalind/rosalind_ba3_test.go
+++ rosalind/rosalind_ba3_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sort"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -32,59 +33,62 @@ func TestKmerComposition(t *testing.T) {
 	}
 }
 
-/// func TestKmerCompositionFile(t *testing.T) {
-///
-/// 	filename := "data/string_composition.txt"
-///
-/// 	// Read the contents of the input file
-/// 	// into a single string
-/// 	lines, err := ReadLines(filename)
-/// 	if err != nil {
-/// 		log.Fatalf("ReadLines: %v", err)
-/// 	}
-///
-/// 	// Input file contents
-/// 	// lines[0]: Input
-/// 	k_str := lines[1]
-/// 	k, _ := strconv.Atoi(k_str)
-///
-/// 	input := lines[2]
-///
-/// 	// lines[3]: Output
-///
-/// 	// Make space for DNA strings
-/// 	gold := make([]string, len(lines)-4)
-/// 	iLstart := 2
-/// 	iLend := len(lines) - 2
-/// 	// Two counters:
-/// 	// one for the line index (iL),
-/// 	// one for the array index (iA).
-/// 	for iA, iL := 0, iLstart; iL < iLend; iA, iL = iA+1, iL+1 {
-/// 		gold[iA] = lines[iL]
-/// 	}
-///
-/// 	results, err := KmerComposition(input, k)
-/// 	if err != nil {
-/// 		t.Error(fmt.Sprintf("Error: %v", err))
-/// 	}
-///
-/// 	// Sort before comparing
-/// 	sort.Strings(gold)
-/// 	sort.Strings(results)
-///
-/// 	if !EqualStringSlices(results, gold) {
-/// 		for i, j := 0, 0; i < len(results); i, j = i+1, j+1 {
-/// 			if results[i] != gold[j] {
-/// 				fmt.Printf("rslt[i] = rslt[%d] = %s\ngold[j] = gold[%d] = %s\n\n",
-/// 					i, results[i], j, gold[j])
-/// 			}
-/// 		}
-/// 		msg := fmt.Sprintf("Error testing KmerComposition() from file %s:\ncomputed = %d\ngold = %d",
-/// 			filename,
-/// 			len(results), len(gold))
-/// 		t.Error(msg)
-/// 	}
-/// }
+func TestKmerCompositionFile(t *testing.T) {
+
+	filename := "data/string_composition.txt"
+
+	// Read the contents of the input file
+	// into a single string
+	lines, err := ReadLines(filename)
+	if err != nil {
+		log.Fatalf("ReadLines: %v", err)
+	}
+
+	// Input file contents
+	// lines[0]: Input
+	k_str := lines[1]
+	k, _ := strconv.Atoi(k_str)
+
+	input := lines[2]
+
+	// lines[3]: Output
+	// lines[4+]: gold standard answers
+
+	// Make space for DNA strings
+	iLstart := 4
+	iLend := len(lines)
+	gold := make([]string, len(lines)-iLstart)
+
+	// Two counters:
+	// one for the line index (iL),
+	// one for the array index (iA).
+	for iA, iL := 0, iLstart; iL < iLend; iA, iL = iA+1, iL+1 {
+		gold[iA] = lines[iL]
+	}
+
+	results, err := KmerComposition(input, k)
+	if err != nil {
+		t.Error(fmt.Sprintf("Error: %v", err))
+	}
+
+	// Check that lengths are equal
+	if len(results) != len(gold) {
+		msg := "Error testing KmerComposition(): length of computed kmer composition does not match gold standard:"
+		msg += fmt.Sprintf("len(computed) = %d, len(gold) = %d\n", len(results), len(gold))
+		t.Error(msg)
+	}
+
+	// Sort before comparing
+	sort.Strings(gold)
+	sort.Strings(results)
+
+	if !EqualStringSlices(results, gold) {
+		msg := fmt.Sprintf("Error testing KmerComposition() from file %s:\ncomputed = %d\ngold = %d",
+			filename,
+			len(results), len(gold))
+		t.Error(msg)
+	}
+}
 
 /////////////////////////////////
 // BA3b Test
